29ff52398325ea80e181fe209872b85e50721ac9
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-05-18 15:01:18 +0530

    code logic complete refactoring time

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const cards = document.querySelectorAll(".memory-card");
 
 
-function flipCard(){
-    this.classList.toggle("flip");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+    } else {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //if cards match
+        if (firstCard.dataset.card === secondCard.dataset.card) {
+            //remove the actionlistners
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            //if cards don't match
+
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000)
+        }
+    }
 }
+/*
+function checkForMatch(){
+    
+}*/
 //we are looping through the cards and adding an eventlistner
 //that event listner is looking for a click event 
 //whenever a card is clicked flipCard will be called
-cards.forEach(card => card.addEventListener("click",flipCard));
+cards.forEach(card => card.addEventListener("click", flipCard));
