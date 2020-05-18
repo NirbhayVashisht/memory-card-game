2ac14a8e50b8b95cde11937ba27176a8684c2c20
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-05-18 15:06:37 +0530

    checkFornatch and disbaleCards refactored

--- app.js
+++ app.js
@@ -18,24 +18,28 @@ function flipCard() {
         secondCard = this;
 
         //if cards match
-        if (firstCard.dataset.card === secondCard.dataset.card) {
-            //remove the actionlistners
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            //if cards don't match
-
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1000)
-        }
+        checkForMatch();
     }
 }
-/*
 function checkForMatch(){
-    
-}*/
+    //if cards match
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        //remove the actionlistners
+        disbaleCards();    
+    } else {
+        //if cards don't match
+
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 1000)
+    }
+}
+
+function disbaleCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
 //we are looping through the cards and adding an eventlistner
 //that event listner is looking for a click event 
 //whenever a card is clicked flipCard will be called
